9e3d3d13d87684f253d5637c24e0c738ab8a8f1a
Habeeb Bolaji <up2132180@myport.ac.uk>
2025-02-21 18:00:03 +0000

    Unfinished

--- index.js
+++ index.js
@@ -12,3 +12,18 @@ async function showMessage(elem, url) {
   const content = await response.text();
   elem.textContent = content;
 }
+
+// 1. Create a function `showList` that takes two parameters: an element and a string that is a URL. 
+// The function will fetch the URL, parse the retrieved data as JSON; the data is guaranteed to be an array of strings.
+//  The function will then, like the `filler` function in `dom101`, put the contents of the array as list items into the provided element.
+
+async function showList(elem, url) {
+  const response = await fetch(url);
+  const  content = await response.json();
+
+  content.forEach(element => {
+    const li = document.createElement('li');
+    li.textContent = element;
+    elem.append(li);
+  });
+}
