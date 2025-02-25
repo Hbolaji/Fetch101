fa606752e4d9a38b13412eed6ce41747174635e5
Habeeb Bolaji <up2132180@myport.ac.uk>
2025-02-25 00:06:12 +0000

    finally finished unit tests

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ async function showMessage(elem, url) {
   elem.textContent = content;
 }
 
-// 1. Create a function `showList` that takes two parameters: an element and a string that is a URL. 
+// 2. Create a function `showList` that takes two parameters: an element and a string that is a URL. 
 // The function will fetch the URL, parse the retrieved data as JSON; the data is guaranteed to be an array of strings.
 //  The function will then, like the `filler` function in `dom101`, put the contents of the array as list items into the provided element.
 
@@ -26,4 +26,42 @@ async function showList(elem, url) {
     li.textContent = element;
     elem.append(li);
   });
+}
+
+// 3.Create a function `startShowingMessage` that takes two parameters: an element and a string that is a URL.
+//  The function will use `setInterval` to make the following task every 1s: fetch the URL and put the response text 
+// into the text content of the provided element. 
+
+async function startShowingMessage(elem, url) {
+  setInterval(async function() {
+    const response = await fetch(url);
+    const content = await response.text()
+    elem.textContent = content;
+ }, 1000);
+}
+
+// 4. Create a function 'handleError' that accepts an element and a url as a parameter, and shows the fetched text from the server in the element. 
+// If there is an error, the function should set the textContent of the element to 'OH DEAR'. Rerun
+async function  handleError(elem, url) {
+  const response = await fetch(url);
+  const content = await response.text();
+  if(!response.ok) {
+    elem.textContent = "OH DEAR";
+  } else {
+    elem.textContent = content;
+  }
+}
+//5. Create a function `drawBox', which accepts two parameters: a canvas element, and a URL which refers to a simple object
+//  with coordinates that you should fetch from a server. The function draws a 10x10 filled black box at the given coordinates. 
+// Your drawBox function should update the coordinates and redraw the box every 1 second.
+
+async function drawBox(canvas, url) {
+  const ctx = canvas.getContext("2d");
+  setInterval(async function () {
+    const response = await fetch(url);
+    const content = await response.json();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "green";
+    ctx.fillRect(content.x, content.y, 30, 30);
+  }, 1000);
 }
